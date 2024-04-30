4f8db6be2a6735a7052f13ee4bb3fc602d1d493c
Vasilis-asmk <a.vasilis.ag@gmail.com>
2024-04-30 13:37:36 +0300

    new classes

---
+++ Παραδοτέο 3/Proejct Code/Project_SE/src/taxi.java
@@ -0,0 +1,26 @@
+public class taxi extends user{
+
+
+    //attributes
+    private int taxiId;
+
+
+    //methods
+    public void rideAccept() {
+
+    }
+
+    public int getTaxiId() {
+        return taxiId;
+    }
+
+
+    // getters and setters
+    public void setTaxiId(int taxiId) {
+        this.taxiId = taxiId;
+    }
+
+    public void notAccept() {
+
+    }
+}
--- Παραδοτέο 3/Proejct Code/Project_SE/src/user.java
+++ Παραδοτέο 3/Proejct Code/Project_SE/src/user.java
@@ -1,2 +1,40 @@
 public class user {
+    private String username;
+    private String email;
+    private String password;
+    private int id;
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }

7ceadb344fa49d3b8aca0c030a91e761c44d7be4
Pantelis-Karagiannis <pantekarag@gmail.com>
2024-05-01 17:48:50 +0300

    part2

---
+++ Παραδοτέο 3/Proejct Code/Project_SE/src/eventCreationScreen.java
@@ -0,0 +1,35 @@
+public class eventCreationScreen extends screen {
+    //attributes
+
+
+    //constructor
+
+    public eventCreationScreen() {
+    }
+
+
+    // methods
+
+    public void checkEventForm() {}
+
+    public void notifyMunicipalEmployee() { System.out.println("Event creation request!"); }
+
+    public void showMisData() {}
+
+    public void showUnavailData() {}
+
+    public void denialInform() { System.out.println("Event creation failed!"); }
+
+    public void createEvent() {}
+
+    public void instertIntoTheDBase() {}
+
+    public void addToEvents() {}
+
+    public void eventCreationCompleted() { System.out.println("Event creation accepted!"); }
+
+
+
+    //getters and setters
+
+}
---
+++ Παραδοτέο 3/Proejct Code/Project_SE/src/storeScreen.java
@@ -0,0 +1,39 @@
+public class storeScreen  extends screen {
+    //attributes
+
+
+    //constructor
+
+    public storeScreen() {
+    }
+
+
+    // methods
+
+    public void showDataForm() {}
+
+    public void checkData() {}
+
+    public void showPayForm() {}
+
+    public void showMisData() {}
+
+    public void checkPayForm() {}
+
+    public void notifyBank()  {}
+
+    public void showMisPayData() {}
+
+    public void showWrongData() {}
+
+    public void showFail() {}
+
+    public void sendReceipt() {}
+
+
+
+
+    //getters and setters
+
+}
+

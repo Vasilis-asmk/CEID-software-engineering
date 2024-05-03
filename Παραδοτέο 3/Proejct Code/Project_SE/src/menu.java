eafda41333eda588adecc5f40524720f4ca30f0d
Pantelis-Karagiannis <pantekarag@gmail.com>
2024-05-04 00:50:59 +0300

    part4

---
+++ Παραδοτέο 3/Proejct Code/Project_SE/src/menu.java
@@ -0,0 +1,34 @@
+public class menu {
+    //attributes
+
+
+    //constructor
+
+    public menu() {
+    }
+
+
+    // methods
+
+    public void createDestForm() {}
+
+    public void notify() {}
+
+    public void intDetForm() {}
+
+    public void createEventForm() {}
+
+    public void showEventList() {}
+
+    public void noAvailEvent() {}
+
+    public void showAvailTrans() {}
+
+    public void createAreaForm() {}
+
+    public void showTs() {}
+
+
+    //getters and setters
+
+}

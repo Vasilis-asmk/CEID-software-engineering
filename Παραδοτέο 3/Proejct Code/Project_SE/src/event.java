22a61ed4dbf8e34f74faf9d7a73844c66453ab35
Pantelis190 <pantelism2002@gmail.com>
2024-05-04 17:19:14 +0300

    Code

---
+++ Παραδοτέο 3/Proejct Code/Project_SE/src/event.java
@@ -0,0 +1,51 @@
+public class event extends eventOrganizer{
+    //attributes
+    private int eventId;
+    private string eventName;
+    private string eventDate;
+    private string eventLocation;
+
+    //constructor
+
+    public event() {
+    }
+
+    //methods
+
+
+    //getters and setters
+
+    public int getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(int eventId) {
+        this.eventId = eventId;
+    }
+
+
+    public string getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(string eventName) {
+        this.eventName = eventName;
+    }
+
+    public string getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(string eventDate) {
+        this.eventDate = eventDate;
+    }
+
+
+    public string getEventLocation() {
+        return eventLocation;
+    }
+
+    public void setEventLocation(string eventLocation) {
+        this.eventLocation = eventLocation;
+    }
+}
---
+++ Παραδοτέο 3/Proejct Code/Project_SE/src/eventReview.java
@@ -0,0 +1,52 @@
+public class eventReview extends event{
+    //attributes
+    public int eventRevId;
+    public string eventRevTitle;
+    public string eventRevText;
+    public int eventRevScore;
+
+    //constructor
+
+    public eventReview() {
+    }
+
+    //methods
+
+
+    //getters and setters
+
+    public int getEventRevId() {
+        return eventRevId;
+    }
+
+    public void setEventRevId(int eventRevId) {
+        this.eventRevId = eventRevId;
+    }
+
+
+    public string getEventRevTitle() {
+        return eventRevTitle;
+    }
+
+    public void setEventRevTitle(string eventRevTitle) {
+        this.eventRevTitle = eventRevTitle;
+    }
+
+
+    public string getEventRevText() {
+        return eventRevText;
+    }
+
+    public void setEventRevText(string eventRevText) {
+        this.eventRevText = eventRevText;
+    }
+
+
+    public int getEventRevScore() {
+        return eventRevScore;
+    }
+
+    public void setEventRevScore(int eventRevScore) {
+        this.eventRevScore = eventRevScore;
+    }
+}

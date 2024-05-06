7feba7575d86120a01f46860cc7a36d748138a64
Pantelis190 <pantelism2002@gmail.com>
2024-05-06 16:09:19 +0300

    code

---
+++ Παραδοτέο 3/Proejct Code/Project_SE/src/transSystemsScreen.java
@@ -0,0 +1,27 @@
+public class transSystemsScreen extends screen {
+    //attributes
+
+
+    //constructor
+
+    public transSystemsScreen() {
+    }
+
+    //methods
+
+    public void showItineraries() {}
+
+    public void chooseBetween() {}
+
+    public void showRoute() {}
+
+    public void notify() {}
+
+    public void calcRoute() {}
+
+    public void showDetails() {}
+
+
+    //getters and setters
+
+}

40ed5bb586938316100f290efabc111a1ace299e
Pantelis190 <pantelism2002@gmail.com>
2024-05-07 12:50:16 +0300

    code

---
+++ Παραδοτέο 3/Proejct Code/Project_SE/src/eventsListScreen.java
@@ -0,0 +1,38 @@
+public class eventsListScreen extends screen {
+    //attributes
+
+
+    //constructor
+
+
+    public eventsListScreen() {
+    }
+
+    //methods
+
+    public void showEventInfo() {}
+
+    public void buyTicketOption() {}
+
+    public void showDataForm() {}
+
+    public void checkDataForm() {}
+
+    public void showPayDataForm() {}
+
+    public void showMisData() {}
+
+    public void checkPayDataForm() {}
+
+    public void notifyBank() {}
+
+    public void showWrongData() {}
+
+    public void showFail() {}
+
+    public void sendReceipt() {}
+
+
+    //getters and setters
+    
+}

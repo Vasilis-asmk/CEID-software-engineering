ac54a6b31b905f714a2d519d91dacbd24ec72166
Pantelis190 <pantelism2002@gmail.com>
2024-05-18 17:13:43 +0300

    code

--- Παραδοτέο 3/Proejct Code/Project_SE/src/eventsListScreen.java
+++ Παραδοτέο 3/Proejct Code/Project_SE/src/eventsListScreen.java
@@ -11,27 +11,27 @@ public class eventsListScreen extends screen {
 
     //methods
 
-    public void showEventInfo() {}
+    public void showEventInfo() { System.out.println("Show information for chosen event:"); }
 
-    public void buyTicketOption() {}
+    public void buyTicketOption() { System.out.println("Buy available tickets for chosen event:"); }
 
-    public void showDataForm() {}
+    public void showDataForm() { System.out.println("Fill in your transaction data: "); }
 
-    public void checkDataForm() {}
+    public void checkDataForm() { System.out.println("Processing Data"); }
 
-    public void showPayDataForm() {}
+    public void showPayDataForm() { System.out.println("Fill in your payment data: "); }
 
-    public void showMisData() {}
+    public void showMisData() { System.out.println("Show missing data of data form:"); }
 
-    public void checkPayDataForm() {}
+    public void checkPayDataForm() { System.out.println("Processing Payment Data"); }
 
-    public void notifyBank() {}
+    public void notifyBank() { System.out.println("Notify bank about transaction"); }
 
-    public void showWrongData() {}
+    public void showWrongData() { System.out.println("Show wrong data of payment form:"); }
 
-    public void showFail() {}
+    public void showFail() { System.out.println("Transaction was not accepted on time ,therefore it was cancelled! "); }
 
-    public void sendReceipt() {}
+    public void sendReceipt() { System.out.println("Payment was successfully accepted. Transaction receipt was sent via email."); }
 
 
     //getters and setters
